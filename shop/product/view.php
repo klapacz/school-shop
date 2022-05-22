58ce15201f84032a29c6e93804d7b68ea85e5925
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-22 16:26:25 +0200

    feat: product not found information

--- shop/product/view.php
+++ shop/product/view.php
@@ -10,12 +10,14 @@ if (empty($_GET['id'])) {
 $sth = $db->prepare('SELECT id, name, price, availability FROM products WHERE id = :id');
 $sth->execute([ "id" => $_GET['id'] ]);
 $product = $sth->fetch(PDO::FETCH_ASSOC);
-
-// TODO: error handling
 ?>
 
-<h1> Product <?= $product["name"] ?></h1>
+<?php if (!$product) : ?>
+    <h1>Product not found</h1>
+<?php else : ?>
+    <h1> Product <?= $product["name"] ?></h1>
 
-<p>Price: <?= $product["price"] / 100 ?>zł</p>
-<p>Availability: <?= $product["availability"] / 100 ?>zł</p>
+    <p>Price: <?= $product["price"] / 100 ?>zł</p>
+    <p>Availability: <?= $product["availability"] / 100 ?>zł</p>
+<?php endif ?>
 
