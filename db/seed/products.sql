6c841c2a1832f42dffae37475f447bd7dbd35fbc
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-23 11:10:18 +0200

    chore: add seed files

---
+++ db/seed/products.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (
+    name,
+    price,
+    availability
+) VALUES (
+    'Sanki',
+    12345,
+    10
+)
---
+++ db/seed/users.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (
+    email,
+    password,
+    first_name,
+    last_name,
+    role
+) VALUES (
+    'admin@admin.com',
+    '$2a$12$MVKQ3iK1D/T.0SOiRSzSDunQphvDcRCZvvsE1aGhDvHemHysRC9zC', -- password
+    'Admin',
+    'Admin',
+    'ADMIN'
+)
