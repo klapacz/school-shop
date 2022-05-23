f9469e7a4b84b42cca7bcad5770524c21805f8a5
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-23 11:18:15 +0200

    fix: product name should be unique

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
 
 CREATE TABLE products (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
+    name varchar(255) NOT NULL UNIQUE,
     price int NOT NULL,
     availability int NOT NULL
 );
