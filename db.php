f5daee64be71e09221ee8568e4a3cdf3e71185e1
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-25 12:07:12 +0200

    refactor: optional env for db connection

--- db.php
+++ db.php
@@ -1,10 +1,14 @@
 <?php
 
-// TODO: use more unique variable names
-$host = 'localhost';
-$dbname = 'shop';
-$user = 'root';
-$password = '';
+function get_db () {
+    $host = isset($_ENV["DB_HOST"]) ? $_ENV["DB_HOST"] : 'localhost';
+    $dbname = isset($_ENV["DB_NAME"]) ? $_ENV["DB_NAME"] : 'shop';
+    $user = isset($_ENV["DB_USER"]) ? $_ENV["DB_USER"] : 'root';
+    $password = isset($_ENV["DB_PASSWORD"]) ? $_ENV["DB_PASSWORD"] : '';
 
-// connect to db with PDO
-$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    // connect to db with PDO
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    return $db;
+}
+
+$db = get_db();
